d3077d88fd97b25896d72fd451887c8b0b86b312
Kailashmandal <hashtagkailash@gmail.com>
2022-02-09 12:06:42 +0530

    post request handling

--- app.js
+++ app.js
@@ -8,33 +8,62 @@ const app=express(); // storing express in app
 const port=  8000 || process.env.PORT;
 const host = '127.0.0.1' || '0.0.0.0';
 
-// get is method used for sending response and accepting only get requstes
-// means when a get request is accepted  than only this response is send with matching url
-app.get('/' , (req, res)=>{
-    res
-    .status(200)
-    .json({
-        message : 'hye from express server !!' ,
-        app : 'express server',
-        content_type : 'json'
-    });
-    // sending the json type data to server
+/**
+ *  adding a middleware here 
+ *  a middleware function that can simpally modify the incomng req data 
+ *  
+ * for example express.json( ) is a middleware here
+ */
+app.use(express.json());
+
+
+/**
+ * JSON.parse is method used to parse the json objects into js objects like json array 
+ *  will get parsed into js array objects and json objects get parsed into js objects
+ *  etc. and etc. 
+ */
+const tours= JSON.parse(
+    fs.readFileSync('./dev-data/data/tours-simple.json' , 'utf-8')
+    );
+
+app.get('/' ,(req, res)=>{
+    res.status(200).send('welcome to our application.');
+})
+
+app.get('/api/v1/tours', (req ,res)=>{
+   res.status(200).json({
+       status : 'success',
+       results : tours.length,
+       data : {
+           tours : tours
+       }
+   })
 });
 
+// handling post requests
+app.post('/api/v1/tours', (req , res)=>{
+    // for posting any data req object contains the data
+       //console.log(req.body);
 
-// making post request response method
-app.post('/' , (req , res)=>{
-    res
-    .status(200)
-    .json({
-        method:'post',
-        urlPath:'/',
-        message: ' this is response for post method'
-    })
-       
+  const newId= tours[tours.length -1].id +1 ;
+  const newTour = Object.assign( {id : newId} , req.body);
+  tours.push(newTour);
+  const newTourJson = JSON.stringify(tours);
+  
+  fs.writeFile('./dev-data/data/tours-simple.json' , newTourJson,(err)=>{
+            res
+            .status(201)
+            .json({
+                status : 'success' ,
+                data : {
+                    tour : newTour
+                }
+            })
+  })
+    //    res.send('done');
 });
 
 // started listening on the port 8000
 app.listen(port ,host, ()=>{
-    console.log('App is runing on port ', port);
+    console.log('server is running at http://127.0.0.1:8000');
 });
